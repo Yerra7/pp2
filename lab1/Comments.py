ad372f7d598ab27a58ac4dfa068bae00a28b5534
Yerra7 <ye_orynbassar@kbtu.kz>
2025-01-25 20:11:54 +0500

    upd

---
+++ lab1/Comments.py
@@ -0,0 +1,28 @@
+# Python Comments
+# Comments are used to explain Python code and make it more readable.
+# They can also be used to prevent code execution during testing.
+
+# Single-line comment example
+print("Hello, World!")  # This is a comment explaining the print statement
+
+# Multiline comments can be done with triple quotes (''' or """ ):
+'''
+This is a multiline comment.
+It is often used to provide more detailed explanations or documentation.
+Python will ignore this entire block of text.
+'''
+
+print("Cheers, Mate!")  # This line prints another greeting
+
+# Using comments to prevent code execution:
+'''
+The following line is commented out and will not run:
+print("Hello, World!")
+'''
+
+# Multiline comment example with a single # on each line:
+# This is another way to write multiline comments.
+# Each line starts with a #
+# This style is less common but still used sometimes for small block comments.
+
+print("Hello, World!")  # This is a simple example of inline comments
