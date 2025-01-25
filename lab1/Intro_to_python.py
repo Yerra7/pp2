92c14260b6f265843db71368cc2d6ecc1d071272
Yerra7 <ye_orynbassar@kbtu.kz>
2025-01-25 19:22:47 +0500

    first commit

---
+++ lab1/Intro_to_python.py
@@ -0,0 +1,30 @@
+'''
+What is Python?
+Python is a popular programming language created by Guido van Rossum in 1991. It’s used for web development, software development, data handling, and scripting.
+
+What Can Python Do?
+Build web apps and workflows.
+Connect to databases, modify files.
+Handle big data and perform complex math.
+Prototype or develop production-ready software.
+
+Why Python?
+Works on multiple platforms (Windows, Mac, Linux).
+Simple, English-like syntax for quick prototyping.
+Supports procedural, object-oriented, and functional programming.
+
+Key Facts:
+Python 3 is the latest version (used in this tutorial).
+It is readable and uses indentation instead of braces for scope.
+
+Example:
+'''
+
+print("Hello, World!")
+
+
+
+
+
+
+
